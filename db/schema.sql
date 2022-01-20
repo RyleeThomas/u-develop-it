5a2af168a1fed1b4768e3516ab02bb5b0892f6b4
rylee thomas <ryleetreat@gmail.com>
2022-01-19 21:21:37 -0500

    modulized

--- db/schema.sql
+++ db/schema.sql
@@ -8,6 +8,14 @@ CREATE TABLE parties (
   description TEXT
 );
 
+/*Creates a header Table*/
+CREATE TABLE voters (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
 
 /* creates a table foundation with the data critieria */
 CREATE TABLE candidates (
